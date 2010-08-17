e7255be7be45026a2090d8dd97b3a73383ef3b43
unknown <Steve Wagner@.(none)>
2010-08-17 17:51:37 +0200

    Minor cleanup.

--- source/MongoDB/Util/ObjectToDocumentConverter.cs
+++ source/MongoDB/Util/ObjectToDocumentConverter.cs
@@ -1,31 +1,26 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-
-namespace MongoDB.Util
+﻿namespace MongoDB.Util
 {
     internal static class ObjectToDocumentConverter
     {
-
         public static Document Convert(object obj)
         {
-            if (obj == null)
+            if(obj == null)
                 return null;
 
-            Document doc = obj as Document;
-            if (doc != null)
+            var doc = obj as Document;
+
+            if(doc != null)
                 return doc;
 
             doc = new Document();
 
-            foreach (var prop in obj.GetType().GetProperties())
+            foreach(var prop in obj.GetType().GetProperties())
             {
-                if (!prop.CanRead)
+                if(!prop.CanRead)
                     continue;
 
-                object value = prop.GetValue(obj, null);
-                if (!TypeHelper.IsNativeToMongo(prop.PropertyType))
+                var value = prop.GetValue(obj, null);
+                if(!TypeHelper.IsNativeToMongo(prop.PropertyType))
                     value = Convert(value);
 
                 doc[prop.Name] = value;
@@ -33,7 +28,5 @@ namespace MongoDB.Util
 
             return doc;
         }
-
-
     }
 }
