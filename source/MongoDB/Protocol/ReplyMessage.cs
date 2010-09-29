a7f62840214732edb9dfe3b29f4cc49c86453314
Steve Wagner <lanwin@lanwin.de>
2010-09-29 08:05:10 +0200

    Add ResponseFlags enum.

--- source/MongoDB/Protocol/ReplyMessage.cs
+++ source/MongoDB/Protocol/ReplyMessage.cs
@@ -37,7 +37,7 @@ namespace MongoDB.Protocol
         /// normally zero, non-zero on query failure     
         /// </summary>
         /// <value>The response flag.</value>
-        public int ResponseFlag { get; set; }
+        public ResponseFlags ResponseFlag { get; set; }
 
         /// <summary>
         /// id of the cursor created for this query response 
@@ -71,7 +71,7 @@ namespace MongoDB.Protocol
             stream = new BufferedStream(stream, 256);
             var reader = new BinaryReader(stream);
             Header = ReadHeader(reader);
-            ResponseFlag = reader.ReadInt32();
+            ResponseFlag = (ResponseFlags)reader.ReadInt32();
             CursorId = reader.ReadInt64();
             StartingFrom = reader.ReadInt32();
             NumberReturned = reader.ReadInt32();
@@ -104,50 +104,5 @@ namespace MongoDB.Protocol
             
             return header;
         }
-
-        //        public void Read(Stream stream){        
-        //            /* Used during debugging of the stream.
-        //            BsonReader headerreader = new BsonReader(stream);
-        //            this.Header = ReadHeader(headerreader);
-        //            
-        //            //buffer the whole response into a memorystream for debugging.
-        //            MemoryStream buffer = new MemoryStream();
-        //            BinaryReader buffReader = new BinaryReader(stream);
-        //            BinaryWriter buffWriter = new BinaryWriter(buffer);
-        //            byte[] body = buffReader.ReadBytes(this.Header.MessageLength - 16);
-        //            System.Console.WriteLine(BitConverter.ToString(body));
-        //            buffWriter.Write(body);
-        //            buffer.Seek(0, SeekOrigin.Begin);
-        //            
-        //            BsonReader reader = new BsonReader(buffer);*/
-        //            
-        //            //BsonReader reader = new BsonReader(stream);
-        //            //BsonReader reader = new BsonReader(new BufferedStream(stream));
-        //            BsonReader reader = new BsonReader(new BufferedStream(stream, 4 * 1024));
-        //            this.Header = ReadHeader(reader);
-        //            
-        //            this.ResponseFlag = reader.ReadInt32();
-        //            this.CursorID = reader.ReadInt64();
-        //            this.StartingFrom = reader.ReadInt32();
-        //            this.NumberReturned = reader.ReadInt32();
-        //
-        //            List<BsonDocument> docs = new List<BsonDocument>();
-        //            for(int num = 0; num < this.NumberReturned; num++){
-        //                BsonDocument doc = new BsonDocument();
-        //                doc.Read(reader);
-        //                docs.Add(doc);
-        //            }
-        //            this.Documents = docs.ToArray();
-        //        }
-        //        
-        //        protected MessageHeader ReadHeader(BsonReader reader){
-        //            MessageHeader hdr = new MessageHeader(OpCode.Reply);
-        //            hdr.MessageLength = reader.ReadInt32();
-        //            hdr.RequestId = reader.ReadInt32();
-        //            hdr.ResponseTo = reader.ReadInt32();
-        //            int op = reader.ReadInt32();
-        //            if((OpCode)op != OpCode.Reply) throw new InvalidDataException("Should have been a reply but wasn't");
-        //            return hdr;
-        //        }
     }
 }
