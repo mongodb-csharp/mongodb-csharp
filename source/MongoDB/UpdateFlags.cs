effb4fbb82e1436d7b9612af0e5bdfe7482bf766
Steve Wagner <lanwin@lanwin.de>
2010-09-29 08:04:04 +0200

    Mark UpdateFlags as Flags.

--- source/MongoDB/UpdateFlags.cs
+++ source/MongoDB/UpdateFlags.cs
@@ -1,26 +1,32 @@
-﻿namespace MongoDB
+﻿using System;
+
+namespace MongoDB
 {
     /// <summary>
-    /// Update flags.
+    ///   Update flags.
     /// </summary>
     /// <remarks>
-    /// Bits 2-31 are Reserved and must be set to 0. 
+    ///   Bits 2-31 are Reserved and must be set to 0.
     /// </remarks>
-    public enum UpdateFlags {
+    [Flags]
+    public enum UpdateFlags
+    {
         /// <summary>
-        /// Default is None.
+        ///   Default is None.
         /// </summary>
         Default = 0,
         /// <summary>
-        /// None.
+        ///   None.
         /// </summary>
         None = 0,
         /// <summary>
-        /// If set, the database will insert the supplied object into the collection if no matching document is found.
+        ///   If set, the database will insert the supplied object into the 
+        ///   collection if no matching document is found.
         /// </summary>
         Upsert = 1,
         /// <summary>
-        /// If set, the database will update all matching objects in the collection. Otherwise only updates first matching doc.
+        ///   If set, the database will update all matching objects in the 
+        ///   collection. Otherwise only updates first matching doc.
         /// </summary>
         MultiUpdate = 2
     }
